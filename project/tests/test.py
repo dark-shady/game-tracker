ebf3d02d061abd3f1190f87e0f132c9be3c336cf
dark-shady <darkshady@zoominternet.net>
2018-03-26 00:14:26 -0400

    Fixed structure

---
+++ project/tests/test.py
@@ -0,0 +1,37 @@
+import os
+import unittest
+
+from project import app
+
+
+TEST_DB = 'test.db'
+
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+
+        self.app = app.test_client()
+        self.app.testing = True 
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+
+###############
+#### tests ####
+###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- run.py
+++ run.py
@@ -1,2 +1,2 @@
-from app import app
+from project import app
 app.run(host='0.0.0.0', port=8080, debug=True)
