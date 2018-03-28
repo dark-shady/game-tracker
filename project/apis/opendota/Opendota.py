1251dec3e37ed165e3b8ff74bc774306af43155e
dark-shady <darkshady@zoominternet.net>
2018-03-27 23:45:05 -0400

    Started DOTA jinja templates

---
+++ project/apis/opendota/Opendota.py
@@ -0,0 +1,55 @@
+import json
+import requests
+from urllib.parse import urljoin
+
+class Opendota:
+    def __init__(self):
+        self.BASE_URL = "https://api.opendota.com/api/"
+
+
+    def make_request(self, api_url):
+        try:
+            r = requests.get(urljoin(self.BASE_URL, api_url))
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print (err)
+
+        return json.loads(r.text)
+
+
+    def get_recent_matches(self, steam_id):
+        return (steam_id, self.get_personaname(steam_id),
+                self.make_request("players/{}/recentMatches".format(steam_id)))
+
+    def get_personaname(self, steam_id):
+        return self.make_request("players/{}".format(steam_id))['profile']['personaname']
+
+
+'''
+https://api.opendota.com/api/players/{account_id}/recentMatches
+'assists': 11,
+          'cluster': 121,
+          'deaths': 2, ********************
+          'duration': 2335, *******************
+          'game_mode': 3,*******************
+          'gold_per_min': 472, *******************
+          'hero_damage': 21742,
+          'hero_healing': 0,
+          'hero_id': 47, *******************
+          'is_roaming': False,
+          'kills': 7, *******************
+          'lane': 2,
+          'lane_role': 2,
+          'last_hits': 164,
+          'leaver_status': 0,
+          'lobby_type': 7, *******************
+          'match_id': 3663098425, *******************
+          'party_size': 3, *******************
+          'player_slot': 1, *******************
+          'radiant_win': True,*******************
+          'skill': None,
+          'start_time': 1515209299,
+          'tower_damage': 5092,
+          'version': 21,
+          'xp_per_min': 571}
+'''
--- project/routes.py
+++ project/routes.py
@@ -1,7 +1,19 @@
 from flask import render_template
 from project import app
+from project.apis.opendota.Opendota import Opendota
+
+
+#################REMOVE######################
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+YOUR_STEAM_ID = 86904765
 
 dota_friends = {'darkshady':86904765, 'lopi-':50122249 }
+#####################END REMOVE##################
+
+# Initialize API wrappers
+opendota_api = Opendota()
+
 
 @app.route('/')
 @app.route('/index')
@@ -9,9 +21,27 @@ def index():
     return render_template('index.html', title='Home')
 
 @app.route('/dota')
-def dota():
+@app.route('/dota/self')
+def dota_self():
+    '''
+        Displays your own most recent DOTA games
+    '''
 
-    return render_template('dota.html', title='DOTA stats')
+    self_recent_matches = opendota_api.get_recent_matches(YOUR_STEAM_ID)
+
+    return render_template('dota_self.html', title='DOTA stats', matches=self_recent_matches)
+
+@app.route('/dota/friends')
+def dota():
+    '''
+        Displays the most recent games of your friends
+    '''
+
+    friends_matches = []
+    for friend in dota_friends.keys():
+        friends_matches.append(opendota_api.get_recent_matches(dota_friends[friend]))
+
+    return render_template('dota_friends.html', title='DOTA stats', friends_matches=friends_matches)
 
 @app.route('/fortnite')
 def fortnite():
